30958afd937829ec504cca24e18f71c9b11eefdf
Michael Neis <michael.neis12@gmail.com>
2021-10-05 00:30:07 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+const list = []
+
+function writeCards(names){
+    for (let i = 0; i < names.length; i++){
+        list.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+
+    return list;
+}
+
+
+
+function countDown(i){
+    while (i >= 0){
+        console.log(i);
+        i--
+    }
+}
+
